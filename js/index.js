2683773fafc0e5446e6aafd94b4d304ba7b202d4
seiso <viktoreydman@gmail.com>
2021-03-24 16:08:02 +0500

    improved responsiveness

--- js/index.js
+++ js/index.js
@@ -1,15 +1,42 @@
 // Splide
 
-const splideOptions = {
-    type   : 'loop',
+var splideOptions = {
+    type: 'loop',
     perPage: 4,
     speed: 300,
     height: "37rem",
+    autoWidth: true,
     perMove: 1,
     autoplay: true,
     interval: 3000,
     pagination: false,
 }
+
+function changeOptionsValue(key, value) {
+    for (var i in splideOptions) {
+        if (splideOptions[i].key == key) {
+            splideOptions[i].key = value;
+            break;
+        }
+    }
+}
+
+// Media queries
+
+const media900px = window.matchMedia('(max-width: 900px');
+
+function handle900PxChange (e) {
+    if (e.matches) {
+        changeOptionsValue("perPage", 2);
+    }
+}
+
+media900px.addListener(handle900PxChange);
+
+handle900PxChange(media900px);
+
+
+
 
 document.addEventListener( 'DOMContentLoaded', function () {
     new Splide( '.best-cards', splideOptions ).mount();
@@ -26,3 +53,4 @@ document.addEventListener( 'DOMContentLoaded', function () {
 document.addEventListener( 'DOMContentLoaded', function () {
     new Splide( '.new-cards', splideOptions ).mount();
 } );
+
